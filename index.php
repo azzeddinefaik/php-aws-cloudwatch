bf3f8b83e27a849ee2cfa0f321a55b0f4a134496
Azzeddine Faik <azzedine.faik@tajawal.com>
2017-03-26 20:45:40 +0400

    updates

--- index.php
+++ index.php
@@ -25,32 +25,5 @@ $client = (new lib\Config())->getCloudWatchClient($conf);
 
 foreach ($conf->metrics as $metrics) {
     foreach ($metrics as $metricName => $metric) {
-        $pluginName = isset($metric->{'plugin'})===true?$metric->{'plugin'}:$metricName;
-        $className = "CloudWatchScript\\Plugins\\" . $pluginName  . "Monitoring";
-        $metricController = new $className($metric,  $metric->name);
-        foreach ($metricController->getAlarms() as $key => $alarm) {
-            $client->putMetricAlarm(array(
-                'AlarmName' => $alarm["Name"],
-                'AlarmDescription' => $metric->description,
-                'ActionsEnabled' => true,
-                'OKActions' => array($conf->alarms->action  ),
-                'AlarmActions' => array($conf->alarms->action ),
-                'InsufficientDataActions' => array($conf->alarms->action),
-                'Dimensions' => array(
-                    array('Name' => 'InstanceId', 'Value' => $instanceId),
-                    array('Name' => 'Metrics', 'Value' => $metricName)
-                ),
-                'MetricName' => $metricController->getMetricName($alarm["Name"]),
-                'Namespace' => $metric->namespace,
-                'Statistic' => 'Average',
-                'Period' => 300,
-                'Unit' => $metricController->getUnit($alarm["Name"]),
-                // EvaluationPeriods is required
-                'EvaluationPeriods' => 2,
-                // Threshold is required
-                'Threshold' => $alarm["Threshold"],
-                // ComparisonOperator is required
-                'ComparisonOperator' => $alarm["ComparisonOperator"]
-            ));
-        }
     }
+}
