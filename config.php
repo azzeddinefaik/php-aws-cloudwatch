0ac16d909cb55fdd833d85f4feebdc066d6c1182
Anmol Nagpal <ianmolnagpal@gmail.com>
2017-03-26 19:09:35 +0400

    update

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 date_default_timezone_set('UTC');
 
-require_once(__DIR__ . '/../vendor/autoload.php');
-include_once(__DIR__ . '/credentials.php');
+require_once(__DIR__ . './vendor/autoload.php');
+include_once(__DIR__ . './credentials.php');
